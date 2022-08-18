e7c7ad51b00a2a8d2f9d9777ff5afb94d5a8d0b8
Chetanbawankule27 <chetanbawankule7@gmail.com>
2022-08-18 09:17:53 +0530

    hellow world class created

---
+++ bootcoding/src/com/bootcoding/basic/Addition.java
@@ -0,0 +1,11 @@
+package com.bootcoding.basic;
+
+public class Addition {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 11;
+        int c = a + b;
+        System.out.println(c);
+    }
+    }
