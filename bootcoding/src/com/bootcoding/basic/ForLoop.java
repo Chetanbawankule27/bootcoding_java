21932a35783de16d79f9ba27b241cd593f50cfab
Chetanbawankule27 <chetanbawankule7@gmail.com>
2022-08-19 08:25:37 +0530

    hellow world class created

---
+++ bootcoding/src/com/bootcoding/basic/ForLoop.java
@@ -0,0 +1,11 @@
+package com.bootcoding.basic;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        int =i;
+        int n=10;
+        for(i=1;i<=n;i++) {
+            System.out.println(i);
+        }
+        }
+}
