ee24217d1f294ac6082a3b51177f071046ec5571
Chetanbawankule27 <chetanbawankule7@gmail.com>
2022-08-19 08:05:47 +0530

    hellow world class created

---
+++ bootcoding/src/com/bootcoding/basic/IfElse.java
@@ -0,0 +1,14 @@
+package com.bootcoding.basic;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a % 10 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+
+
+}
