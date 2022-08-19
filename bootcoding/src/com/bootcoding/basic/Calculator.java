cde7e8b3d7f220ad4acdd41bb122317fd6e0d8d2
Chetanbawankule27 <chetanbawankule7@gmail.com>
2022-08-19 09:25:10 +0530

    calculator

---
+++ bootcoding/src/com/bootcoding/basic/Calculator.java
@@ -0,0 +1,24 @@
+package com.bootcoding.basic;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println(calculator(10, 20, "ADD"));
+        System.out.println(calculator(10, 20, "MUL"));
+        System.out.println(calculator(10, 20, "SUB"));
+        System.out.println(calculator(10, 20, "DIV"));
+    }
+
+    static int calculator(int a, int b, String action) {
+        if (action == "ADD") {
+            return a + b;
+        } else if (action == "MUL") {
+            return a * b;
+        } else if (action == "SUB") {
+            return a - b;
+        } else if (action == "DIV") {
+            return a / b;
+        }
+        return a;
+    }
+
+}
