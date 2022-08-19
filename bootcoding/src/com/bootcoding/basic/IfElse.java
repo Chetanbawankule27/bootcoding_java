67a169385993170771bf68741290dcb5b4909a83
Chetanbawankule27 <chetanbawankule7@gmail.com>
2022-08-19 08:06:16 +0530

    hellow world class created

--- bootcoding/src/com/bootcoding/basic/IfElse.java
+++ bootcoding/src/com/bootcoding/basic/IfElse.java
@@ -3,7 +3,7 @@ package com.bootcoding.basic;
 public class IfElse {
     public static void main(String[] args) {
         int a = 10;
-        if (a % 10 == 0) {
+        if (a % 2== 0) {
             System.out.println("even");
         } else {
             System.out.println("odd");
