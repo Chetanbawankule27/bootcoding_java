5c24f3dab40d0e46fa4d1432e6095e75f6c01861
Chetanbawankule27 <chetanbawankule7@gmail.com>
2022-09-11 13:39:46 +0530

    String Handeling

---
+++ bootcoding/src/com/bootcoding/oops/Cat.java
@@ -0,0 +1,14 @@
+package com.bootcoding.oops;
+
+public class Cat extends Animal {
+    String type;
+
+    public void jump() {
+        System.out.println("i am jumping DND");
+        eat();
+
+    }
+        public void sleep() {
+            System.out.println("i am sleeping animal DND");
+        }
+}
