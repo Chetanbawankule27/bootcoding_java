a40f2391392b8cc914c277d4b8d7c8a18f8efd0b
Chetanbawankule27 <chetanbawankule7@gmail.com>
2022-09-23 09:06:03 +0530

    String Handeling

---
+++ bootcoding/src/com/bootcoding/dsa/linklist/LinklistExample2.java
@@ -0,0 +1,28 @@
+package com.bootcoding.dsa.linklist;
+
+public class LinklistExample2 {
+    public static void main(String[] args) {
+
+        Node head = new Node();
+        head.data = 10;
+        head.next = null;
+        Node current = head;
+        for (int i = 1; i <= 10; i++) {
+
+            Node n1 = new Node();
+            n1.data = i * 10;
+            // n1.next = null;
+            current.next = n1; // assign new element to next of current element
+            current = current.next; // shift to next element
+
+        }
+        current = head.next;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next; // shift to next element
+        }
+    }
+}
+
+
+
---
+++ bootcoding/src/com/bootcoding/dsa/linklist/LinklistExample4.java
@@ -0,0 +1,5 @@
+package com.bootcoding.dsa.linklist;
+
+public class LinklistExample4 {
+
+}
